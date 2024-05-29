7b49bc1c49c1bd0dac54934f64c5b51be84499f3
22ssison <22ssison@wakatipu.school.nz>
2024-05-29 12:16:50 +1200

    list of movie output is working

--- main.py
+++ main.py
@@ -7,44 +7,48 @@ conn = sqlite3.connect("./movies_internal.db")
 # Create a cursor for python to 'type' commands into
 cursor = conn.cursor()
 
-#start creating define statements to execute different querries
-def get_all_movies():
-   cursor.execute("SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies;") 
+#start creating define statements that execute different querries
+
+
+# To show the user the list of different movies that is available.
+def get_all_movie_titles():
+   cursor.execute("SELECT id, title FROM movies;") 
    #save the result to a variable.
    movies = cursor.fetchall()
    #list of tuples
-   print("movies")
-  
-def get_movie_by_title():
+   for movie in movies:
+      print()
+      print(f"{movie[0]}: {movie[1]}")
+      # print(movie)
+
+def get_movie_info_by_title():
    user_title = input("Which movie would you like to search for?\n> ") #.lower 
-   query = f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies FROM movies WHERE name = '{user_title}'"
+   query = f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies WHERE name = '{user_title}'"
    # this query does not convert the user input into a string but rather an sql query
-   
 
 def get_movie_by_genre():
    user_genre = input("Which genre would you like to search for?\n> ") #.lower
-   query = f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies FROM movies WHERE name = '{user_genre}'"
+   query = f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies WHERE name = '{user_genre}'"
    # this query does not convert the user input into a string but rather an sql query
 
-# # Delete these two functions and save it for next trial - create a more simple version
-# # Consider user's input (preferance?)
-# def movies_with_duration_less_than_2_hours():
-#    cursor.execute(f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies WHERE duration > {120} ")
-#    #we can potentially feed it a bunch of data types and store it anyway - input validation 
-#    #fix this to get the user to input a duration instead. 
+# Delete these two functions and save it for next trial - create a more simple version
+# Consider user's input (preferance?)
+def movies_with_duration_less_than_2_hours():
+   cursor.execute(f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies WHERE duration > {120} ")
+   #we can potentially feed it a bunch of data types and store it anyway - input validation 
+   #fix this to get the user to input a duration instead. 
 
-# def movies_with_duration_2_hours_or_more():
-#    cursor.execute(f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies WHERE duration <= {120}")
+def movies_with_duration_2_hours_or_more():
+   cursor.execute(f"SELECT title, blurb, genre, actor, duration, director, budget, cinema, rating, year FROM movies WHERE duration <= {120}")
 
 
-#make this more user friendly by greating them first and telling them what this program is about. include import time and put emojies (star options) for movies that are a "must watch."
+# #make this more user friendly by greating them first and telling them what this program is about. include import time and put emojies (star options) for movies that are a "must watch."
 # HEART = chr(9829)
 # print(HEART)
 
-# while True:
-# include this in second test. So that the user will be able to get asked again and again. 
-# make this more user friendly especially for f
+#while loop so that this program can be user friendly and ask the user again when they type something unrecognisable. 
 
+# # this whole entire code block needs work on. 
 # while True:
 #    search_option = input("1. Get all movies\n2. Get specific movie title\n3. Get by movie genre\n4. Exit")
 #    if search_option == "1": #or search_option == "one":
@@ -62,7 +66,14 @@ def get_movie_by_genre():
 #    else:
 #       print("I don't recognise that, please choose option 1, 2, 3 or 4:")
 
-
-
 #they can see all movie names, they can see all movies from highest to lowest rated, they can see all movies 
-print(Showing:)
+
+
+# Search this by numbers aranging each title -- use the id for its number. 
+#ex. 1. Avatar
+
+print("This program will give you information about different movies that is showing in our cinema.")
+print()
+print()
+print("Here is a list of all the movies showing:")
+get_all_movie_titles()
